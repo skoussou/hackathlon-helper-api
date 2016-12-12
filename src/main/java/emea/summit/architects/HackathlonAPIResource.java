7bd8e1b7cddd2646f6c996076c28b8ef11253360
Stelios Koussouris <skousou@gmail.com>
2016-12-12 17:09:12 +0000

    Correcting Swagger documentation

--- src/main/java/emea/summit/architects/HackathlonAPIResource.java
+++ src/main/java/emea/summit/architects/HackathlonAPIResource.java
@@ -199,7 +199,7 @@ public class HackathlonAPIResource {
   @POST  
   @Path("/service/email-santa")
   @Consumes("application/json")
-  @ApiOperation("Sends the email to Santa with the list")
+  @ApiOperation("Sends the email to a list of participants, with subject and payload")
   public String sendEmailNotification(List<RequestPayload> request, String subject, List<String> emailList) {
   	
   	try {
